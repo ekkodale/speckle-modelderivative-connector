e0688d2baf7ac3d1047b195264bcdd2b338cb8c3
Baris Basa <46998262+barocuda@users.noreply.github.com>
2022-05-12 13:56:20 +0200

    Setup backend

---
+++ Speckle.ModelDerivative.Connector.Api/Speckle.ModelDerivative.Connector.Api/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.OpenApi.Models;
+using Speckle.Core.Credentials;
+using Speckle.Core.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// ------------------------------------------------------------------------------------
+//  Configure swagger
+// ------------------------------------------------------------------------------------
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Speckle Model Derivative Connector API",
+        Description = "An ASP.NET Core Web API for Speckle Model Derivative Connector",
+    });
+});
+
+// ------------------------------------------------------------------------------------
+//  Configuration.
+// ------------------------------------------------------------------------------------
+builder.Configuration
+         .AddJsonFile("appsettings.json", false, true)
+         .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+         .AddEnvironmentVariables();
+
+// ------------------------------------------------------------------------------------
+//  Configure speckle.
+// ------------------------------------------------------------------------------------
+string serverUrl = builder.Configuration["Speckle:Server"];
+string token = builder.Configuration["Speckle:PersonalAccessToken"];
+
+ServerInfo serverinfo = await AccountManager.GetServerInfo(serverUrl);
+UserInfo userinfo = await AccountManager.GetUserInfo(token, serverUrl);
+
+var account = new Account { isDefault = true, token = token, serverInfo = serverinfo, userInfo = userinfo };
+var speckleClient = new Client(account);
+
+builder.Services.AddSingleton(speckleClient);
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+   
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+app.UseAuthorization();
+
+app.Run();
