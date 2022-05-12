96540e0a27667aa2a1be3d780c171b47b6060173
ManuelGerhardt <manuel.gerhardt@ekkodale.com>
2022-05-12 15:50:29 +0200

    WIP: working on sending meshes

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,28 +1,32 @@
 //Styling
-import CSS from "csstype"
+import CSS from "csstype";
 //Components
-import ForgeViewer from "./ForgeViewer"
+import ForgeViewer from "./ForgeViewer";
 //Extensions
-import { ModelUrnExtensionHelper } from "./ModelUrnExtensionHelper";
+import {ModelUrnExtensionHelper} from "./ModelUrnExtensionHelper";
+import {MeshExtensionHelper} from "./MeshExtensionHelper";
 
 const forgeViewerContainer: CSS.Properties = {
 	paddingLeft: "5vh",
-    height: "90%"
+	height: "90%",
 };
 
 const forgeViewer: CSS.Properties = {
 	border: "1px solid #1F85DE",
 	height: "90%",
-    position: "relative",
+	position: "relative",
 	width: "98%",
 };
 
 export const Dashboard = () => {
+	const handleModelChange = () => {
+		const inputValue: string = (document.getElementById("modelUrn") as HTMLInputElement).value;
+		ModelUrnExtensionHelper.getInstance().loadNewModel(inputValue);
+	};
 
-    const handleModelChange = () => {
-        const inputValue: string = (document.getElementById('modelUrn') as HTMLInputElement).value
-        ModelUrnExtensionHelper.getInstance().loadNewModel(inputValue);
-    }
+	const testFunc = () => {
+		MeshExtensionHelper.getInstance().testFunc();
+	};
 
 	return (
 		<div className="container-fluid" style={{height: "100vh"}}>
@@ -38,15 +42,21 @@ export const Dashboard = () => {
 							</div>
 						</div>
 					</div>
+					<button className="btn btn-primary" type="button" onClick={() => testFunc()}>
+					Change
+				</button>
 				</div>
-				<div className="col-sm-9" >
-                    <h1 className="h1" style={{marginTop: "1%"}}>Title</h1>
+
+				<div className="col-sm-9">
+					<h1 className="h1" style={{marginTop: "1%"}}>
+						Title
+					</h1>
 				</div>
 			</div>
 			<div className="row" style={forgeViewerContainer}>
-                <div className="col-sm-12 px-0" style={forgeViewer}>
-                    <ForgeViewer urn={"urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3BlY2tsZWhhY2thdGhvbi9yYWNfYmFzaWNfc2FtcGxlX3Byb2plY3QucnZ0"} />
-                </div>
+				<div className="col-sm-12 px-0" style={forgeViewer}>
+					<ForgeViewer urn={"urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3BlY2tsZWhhY2thdGhvbi9yYWNfYmFzaWNfc2FtcGxlX3Byb2plY3QucnZ0"} />
+				</div>
 			</div>
 		</div>
 	);
---
+++ client/src/components/MeshExtension.ts
@@ -0,0 +1,29 @@
+import {MeshExtensionHelper} from "./MeshExtensionHelper";
+
+export class MeshExtension extends Autodesk.Viewing.Extension {
+	constructor(viewer: any, options: any) {
+		super(viewer, options);
+		Autodesk.Viewing.Extension.call(this, viewer, options);
+		const s1 = MeshExtensionHelper.getInstance();
+		s1.addExtension(this);
+	}
+
+	load(): boolean {
+		console.log("ModelUrnExtension loaded!");
+		return true;
+	}
+
+	unload(): boolean {
+		return true;
+	}
+
+	testFunc() {
+		const models: Autodesk.Viewing.Model[] = this.viewer.getAllModels();
+		const model: Autodesk.Viewing.Model = models[0];
+		let fragIds: any = [];
+		let instanceTree = model.getData().instanceTree;
+		instanceTree.fragList.fragments.fragId2dbId.forEach((element: number) => {
+			console.log(this.viewer.impl.getRenderProxy(this.viewer.model, 0));
+		});
+	}
+}
