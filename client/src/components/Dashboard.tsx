aa3cecba4dbb54a0d44b28109a80aaad15e06e9f
edherwin <eduard.herwin@ekkodale.com>
2022-05-13 12:53:58 +0200

    FEATURE: Implemented commit api call

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -4,79 +4,108 @@ import CSS from "csstype";
 import ForgeViewer from "./ForgeViewer";
 import StreamSelect from "./StreamSelect";
 //Extensions
-import {ModelUrnExtensionHelper} from "./ModelUrnExtensionHelper";
-import {MeshExtensionHelper} from "./MeshExtensionHelper";
+import { ModelUrnExtensionHelper } from "./ModelUrnExtensionHelper";
+import { MeshExtensionHelper } from "./MeshExtensionHelper";
 //Redux
 import { DashboardState } from "../context/DashboardReducer";
 import { useSelector } from "react-redux";
 
 const forgeViewerContainer: CSS.Properties = {
-	paddingLeft: "5vh",
-	height: "90%",
+  paddingLeft: "5vh",
+  height: "90%",
 };
 
 const forgeViewer: CSS.Properties = {
-	border: "1px solid #1F85DE",
-	height: "90%",
-	position: "relative",
-	width: "98%",
+  border: "1px solid #1F85DE",
+  height: "90%",
+  position: "relative",
+  width: "98%",
 };
 
 export const Dashboard = () => {
-	const handleModelChange = () => {
-		const inputValue: string = (document.getElementById("modelUrn") as HTMLInputElement).value;
-		ModelUrnExtensionHelper.getInstance().loadNewModel(inputValue);
-	};
-	const viewerInitialized: any = useSelector<DashboardState>(
-		(state: any) => state.dashboardReducer.viewerInitialized
-	);
+  const handleModelChange = () => {
+    const inputValue: string = (
+      document.getElementById("modelUrn") as HTMLInputElement
+    ).value;
+    ModelUrnExtensionHelper.getInstance().loadNewModel(inputValue);
+  };
+  const viewerInitialized: any = useSelector<DashboardState>(
+    (state: any) => state.dashboardReducer.viewerInitialized
+  );
 
-	const testFunc = () => {
-		MeshExtensionHelper.getInstance().testFunc();
-	};
+  const commitFunc = () => {
+    const selectValue: string = (
+      document.getElementById("streamID") as HTMLSelectElement
+    ).value;
+    MeshExtensionHelper.getInstance().commitFunc(selectValue);
+  };
 
-	return (
-		<div className="container-fluid" style={{height: "100vh"}}>
-			<div className="row" style={{height: "10%", paddingTop: "2%"}}>
-				<div className="col-sm-3">
-					<div className="container" style={{marginLeft: "5%"}}>
-						<div className="input-group mb-3">
-							<input type="text" disabled={!viewerInitialized} className="form-control" id="modelUrn" placeholder="Model URN" aria-label="ModelUrn" aria-describedby="basic-addon2" />
-							<div className="input-group-append">
-								<button disabled={!viewerInitialized} className="btn btn-outline-primary" type="button" onClick={() => handleModelChange()}>
-									Change
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="col-sm-4 px-0">
-					<div className="container-fluid row">
-						<StreamSelect/>
-						<button
-							className="btn btn-outline-primary waves-effect"
-							type="button"
-							style={{display: "inline-block", width: "6vw"}}
-							//   onClick={() => handleModelChange()}
-							disabled={!viewerInitialized}
-						>
-							Commit
-							<i className="bi bi-check-lg" style={{float: "right"}}></i>
-						</button>
-					</div>
-				</div>
-				<div className="col-sm-5">
-					<h1 className="h3" style={{textDecoration: "underline", textDecorationColor: "#0080ff", color: "#3c414f"}}>
-						Speckle Model Derivative Connector
-					</h1>
-				</div>
-			</div>
-			<div className="row" style={forgeViewerContainer}>
-				<div className="col-sm-12 px-0" style={forgeViewer}>
-					<ForgeViewer urn={"urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3BlY2tsZWhhY2thdGhvbi9yYWNfYmFzaWNfc2FtcGxlX3Byb2plY3QucnZ0"} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container-fluid" style={{ height: "100vh" }}>
+      <div className="row" style={{ height: "10%", paddingTop: "2%" }}>
+        <div className="col-sm-3">
+          <div className="container" style={{ marginLeft: "5%" }}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                disabled={!viewerInitialized}
+                className="form-control"
+                id="modelUrn"
+                placeholder="Model URN"
+                aria-label="ModelUrn"
+                aria-describedby="basic-addon2"
+              />
+              <div className="input-group-append">
+                <button
+                  disabled={!viewerInitialized}
+                  className="btn btn-outline-primary"
+                  type="button"
+                  onClick={() => handleModelChange()}
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-4 px-0">
+          <div className="container-fluid row">
+            <StreamSelect />
+            <button
+              className="btn btn-outline-primary waves-effect"
+              type="button"
+              style={{ display: "inline-block", width: "6vw" }}
+              onClick={() => commitFunc()}
+              disabled={!viewerInitialized}
+            >
+              Commit
+              <i className="bi bi-check-lg" style={{ float: "right" }}></i>
+            </button>
+          </div>
+        </div>
+        <div className="col-sm-5">
+          <h1
+            className="h3"
+            style={{
+              textDecoration: "underline",
+              textDecorationColor: "#0080ff",
+              color: "#3c414f",
+            }}
+          >
+            Speckle Model Derivative Connector
+          </h1>
+        </div>
+      </div>
+      <div className="row" style={forgeViewerContainer}>
+        <div className="col-sm-12 px-0" style={forgeViewer}>
+          <ForgeViewer
+            urn={
+              "urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6c3BlY2tsZWhhY2thdGhvbi9yYWNfYmFzaWNfc2FtcGxlX3Byb2plY3QucnZ0"
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
--- client/src/components/MeshExtension.ts
+++ client/src/components/MeshExtension.ts
@@ -1,31 +1,95 @@
-import {MeshExtensionHelper} from "./MeshExtensionHelper";
+import {
+  Client,
+  OkResult,
+  Va3cGeometry,
+  Va3cGeometryData,
+  Va3cMaterial,
+  Va3cObject,
+} from "../api/client";
+import Config from "../Config";
+import { MeshExtensionHelper } from "./MeshExtensionHelper";
 
 export class MeshExtension extends Autodesk.Viewing.Extension {
-	constructor(viewer: any, options: any) {
-		super(viewer, options);
-		Autodesk.Viewing.Extension.call(this, viewer, options);
-		const s1 = MeshExtensionHelper.getInstance();
-		s1.addExtension(this);
-	}
+  constructor(viewer: any, options: any) {
+    super(viewer, options);
+    Autodesk.Viewing.Extension.call(this, viewer, options);
+    const s1 = MeshExtensionHelper.getInstance();
+    s1.addExtension(this);
+  }
 
-	load(): boolean {
-		console.log("ModelUrnExtension loaded!");
-		return true;
-	}
+  load(): boolean {
+    console.log("ModelUrnExtension loaded!");
+    return true;
+  }
 
-	unload(): boolean {
-		return true;
-	}
+  unload(): boolean {
+    return true;
+  }
 
-	testFunc() {
-		const models: Autodesk.Viewing.Model[] = this.viewer.getAllModels();
-		const model: Autodesk.Viewing.Model = models[0];
-		let fragIds: any = [];
-		let instanceTree = model.getData().instanceTree;
-		instanceTree.fragList.fragments.fragId2dbId.forEach((element: number) => {
-			console.log(this.viewer.impl.getRenderProxy(this.viewer.model, 0));
-			console.log(this.viewer.impl.getRenderProxy(this.viewer.model, 100));
-			return;
-		});
-	}
+  commitFunc(streamID: string) {
+    const client: Client = new Client(Config.BaseURL);
+    const models: Autodesk.Viewing.Model[] = this.viewer.getAllModels();
+    const model: Autodesk.Viewing.Model = models[0];
+    let instanceTree = model.getData().instanceTree;
+    let fragIds: Array<any> = [];
+    let va3cObjectList: Va3cObject[] = [];
+
+    instanceTree.fragList.fragments.fragId2dbId.forEach((dbId: number) => {
+      instanceTree.enumNodeFragments(dbId, function (fragId: any) {
+        fragIds.push(fragId);
+      });
+    });
+
+    fragIds.forEach((fragId: number) => {
+      let renderProxy = this.viewer.impl.getRenderProxy(model, fragId);
+      let renderProxyGeometry = renderProxy.geometry;
+      let renderProxyMaterial = renderProxy.material;
+      let renderProxyMatrix = renderProxy.matrix;
+
+      // Init Vac3Object
+      let vac3Object: Va3cObject = new Va3cObject();
+
+      vac3Object.geometry = new Va3cGeometry();
+      vac3Object.geometry.data = new Va3cGeometryData();
+
+      vac3Object.material = new Va3cMaterial();
+
+      vac3Object.matrix = [];
+
+      // Assign geometry
+      vac3Object.geometry.data.faces = renderProxyGeometry.ib;
+      vac3Object.geometry.data.vertices = renderProxyGeometry.vb;
+
+      // Assign material
+      vac3Object.material.uuid = renderProxyMaterial.uuid;
+      vac3Object.material.name = renderProxyMaterial.name;
+      vac3Object.material.type = renderProxyMaterial.type;
+      vac3Object.material.color = renderProxyMaterial.color;
+      vac3Object.material.ambient = renderProxyMaterial.ambient;
+      vac3Object.material.emissive = renderProxyMaterial.emissive;
+      vac3Object.material.specular = renderProxyMaterial.specular;
+      vac3Object.material.shininess = renderProxyMaterial.shininess;
+      vac3Object.material.opacity = renderProxyMaterial.opacity;
+      vac3Object.material.transparent = renderProxyMaterial.transparent;
+      vac3Object.material.wireframe = renderProxyMaterial.wireframe;
+
+      // Assign matrix
+      vac3Object.matrix = renderProxyMatrix;
+
+      va3cObjectList.push(vac3Object);
+    });
+
+    console.log("START COMMIT");
+    console.log(va3cObjectList);
+
+    client
+      .commits(streamID, va3cObjectList)
+      .then((response: OkResult) => {
+        console.log("Commited successfully!");
+        console.log(response);
+      })
+      .catch((error: any) => {
+        console.log("Something went wrong with the commit: " + error);
+      });
+  }
 }
